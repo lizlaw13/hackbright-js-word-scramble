fed4b719c7080d6da59cd191f91ebeea9ba7b853
Hackbright Student <no-reply@hackbrightacademy.com>
2019-01-31 17:57:59 -0800

    second save

--- js-word-scramble.js
+++ js-word-scramble.js
@@ -13,3 +13,28 @@ let newCopy = newItems[second];
 newItems[first] = newCopy;
 newItems[second] = Copy;
 console.log(newItems)
+
+let candy = new Map();
+candy['grape'] = 'purple';
+candy['cherry'] = 'red';
+candy['orange'] = 'orange';
+candy['blueberry'] = 'blue;
+candy['mystery'] = 'white';
+
+console.log(candy);
+
+
+let value;
+function candyMap(color, map) {
+  Object.keys(candy).forEach(function (key) {
+    value = candy[key];
+      if (color === value) 
+        let found = true{
+        console.log(`The ${key} flavor is colored ${value}.`);
+      } 
+    })
+  if (found === false) {
+    console.log('Sorry, that color doesn’t have a flavor');
+  }
+  };
+
